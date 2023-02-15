634a2b786fce08248465579bd818a991711e5cf8
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-02-15 14:28:37 -0500

    Get - /error

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT
-
-app.listen(port, () => {
-   console.clear()
-   console.log(`Server listening on port ${port} `)
- })
+const port = process.env.PORT || 3000
 
 // hello
 app.get('/hello', (req,res) => {
@@ -16,7 +11,21 @@ app.get('/hello', (req,res) => {
 
 // status
 app.get('/status', (req,res) => {
-   res.send(`The Environment Name: ${process.env.NAME} - Port Number: ${process.env.PORT}`)
+  res.send(`The Environment Name: ${process.env.NAME} - Port Number: ${process.env.PORT}`)
 })
 
 // error
+app.get('/error', (req, res) => {
+  const error = new Error("Error Message")
+  error.status = 403
+  res.status(error.status).send(`Something goes wrong! - Error code: ${error.status}`)
+})
+
+// email-list
+
+
+
+app.listen(port, () => {
+  console.clear()
+  console.log(`Server listening on port ${port} `)
+})
