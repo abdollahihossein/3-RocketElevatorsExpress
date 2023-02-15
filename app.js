481b745b7afe59dc595ff43e183689ec35c499d9
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-02-14 21:24:21 -0500

    Creating GET -/hello and GET -/status

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT
+
 app.listen(port, () => {
-  console.log(` server listening on port ${port} `)
+   console.clear()
+   console.log(`Server listening on port ${port} `)
+ })
+
+// hello
+app.get('/hello', (req,res) => {
+  res.send('Hello World!')
+  console.log(`Port in use is: ${port}`)
 })
+
+// status
+app.get('/status', (req,res) => {
+   res.send(`The Environment Name: ${process.env.NAME} - Port Number: ${process.env.PORT}`)
+})
+
+// error
