fa264364f61370a6610f194559a715ecd648b95c
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-02-16 13:38:34 -0500

    Creating /calc-residential and /contact-us API

--- app.js
+++ app.js
@@ -61,11 +61,49 @@ app.get('/region-avg', (req, res) => {
     else {
       let avgRating = sumRating/j
       let avgFee = sumFee/j
-      res.send(`The average rating is ${avgRating.toFixed(2)} and the average fee is ${avgFee.toFixed(2)} for agents in the region ${region}`)
+      res.json({
+        region: `${region}`,
+        average_rating: `${avgRating.toFixed(2)}`,
+        average_fee: `${avgFee.toFixed(2)}`})
     }    
   })
 })
 
+// calc-residential
+app.get('/calc-residential', (req, res) => {
+  let numberOfApartments = req.query.numberOfApartments
+  let numberOfFloors = req.query.numberOfFloors
+  let tier = req.query.tier
+  let numberOfBanks = Math.ceil(numberOfFloors/20)
+  let numberOfApartPerFloor = Math.ceil(numberOfApartments/numberOfFloors)
+  let numberOfElevators = numberOfBanks * Math.ceil(numberOfApartPerFloor/6)
+  if (tier == 'standard') {
+    unitPrice = 8000
+    percentage = 0.1
+  }
+  else if (tier == 'premium') {
+      unitPrice = 12000
+      percentage = 0.15
+  }
+  else if (tier == 'excelium') {
+      unitPrice = 15000
+      percentage = 0.2
+  }
+  let installationFee = percentage * numberOfElevators * unitPrice
+  let finalCost = installationFee + numberOfElevators * unitPrice
+  res.json({
+    number_of_elevators_required: `${numberOfElevators}`,
+    total_cost: `${finalCost}`
+  })
+})
+
+
+// contact-us
+app.use(express.json()) // This code enales us to read request body data 
+app.post('/contact-us', (req, res) => {
+  console.log(req.body)
+  res.send(`A message was received from ${req.body.first_name} ${req.body.last_name}`)
+})
 
 app.listen(port, () => {
   console.clear()
