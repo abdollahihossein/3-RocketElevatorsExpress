03185dbe9da68321223d6c87abbfd4dfddc9e752
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-02-16 07:02:24 -0500

    Creating GET API - region-avg

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const fs = require('fs');
+const fs = require('fs')
 require('dotenv').config()
 const port = process.env.PORT || 3000
-
+let agents
+let j
 
 // hello
 app.get('/hello', (req,res) => {
@@ -26,22 +27,44 @@ app.get('/error', (req, res) => {
 // email-list
 app.get('/email-list', (req, res) => {
   let emaillist =[]
-  let agents
-  let j
   fs.readFile('agents.json', (err, data) => {
     if (err) throw err
     agents = JSON.parse(data)
     j = 0
     agents.forEach(element => {
-      emaillist[j] = agents[j].email
+      emaillist[j] = element.email
       j++
     });
     res.send(JSON.stringify(emaillist))
   })
 })
 
-
-
+// region-avg
+app.get('/region-avg', (req, res) => {
+  let region = req.query.region
+  fs.readFile('agents.json', (err, data) => {
+    if (err) throw err
+    agents = JSON.parse(data)
+    let sumRating = 0
+    let sumFee = 0
+    j = 0
+    agents.forEach(element => {
+      if (element.region == region) {
+        sumRating += parseInt(element.rating)
+        sumFee += parseInt(element.fee)
+        j++
+      }
+    })
+    if (j == 0) {
+      res.send(`No agents found in region ${region}`)
+    }
+    else {
+      let avgRating = sumRating/j
+      let avgFee = sumFee/j
+      res.send(`The average rating is ${avgRating.toFixed(2)} and the average fee is ${avgFee.toFixed(2)} for agents in the region ${region}`)
+    }    
+  })
+})
 
 
 app.listen(port, () => {
